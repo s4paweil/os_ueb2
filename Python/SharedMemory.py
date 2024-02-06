e10e468775932cea89a953bf08c92bcc950e7382
s4paweil <P.Weil94@gmail.com>
2024-02-06 21:45:49 +0100

    Keine Ahnung was geändert wurde

--- Python/SharedMemory.py
+++ Python/SharedMemory.py
@@ -36,29 +36,44 @@ class BankServer:
             return result_string
 
 
-def run_server(queue, num_accounts, min_starting_balance, max_starting_balance, seed):
+def print_array_elements(array, n):
+    print(f"[{', '.join(map(str, array[:n]))}]")
+
+def run_server(shm, num_accounts, min_starting_balance, max_starting_balance, seed, num_clients):
     server = BankServer(num_accounts, min_starting_balance, max_starting_balance, seed)
 
+    finished_clients = 0
     transaction_counter = 0
-    while True:
-        data = queue.get()
-        if data == "STOP":
-            print(server.accounts_to_string())
-            break
-        k1, k2, amount = data
-        server.transfer(k1, k2, amount)
-        transaction_counter += 1
+    while finished_clients < num_clients:
+        finished_clients = 0
+        for i in range(num_clients):
+            if shm[i] == 1:
+                start_index = ((i*3) + num_clients)
+                k1, k2, amount = shm[start_index:start_index+3]
+
+                server.transfer(k1, k2, amount)
+                transaction_counter += 1
+                shm[i] = 0
+            if shm[i] == -1:
+                finished_clients += 1
+
+    print(server.accounts_to_string())
 
 
-
-def run_client(queue, num_operations, num_accounts, min_transfer_amount, max_transfer_amount, seed):
+def run_client(shm, num_operations, num_accounts, min_transfer_amount, max_transfer_amount, seed, id, num_clients):
     random_generator = LCG(str(seed))
 
-    for _ in range(num_operations):
-        k1 = random_generator.get_next_number(num_accounts) + 1
-        k2 = random_generator.get_next_number(num_accounts) + 1
-        amount = random_generator.get_next_number_between(min_transfer_amount, max_transfer_amount)
-        queue.put((k1, k2, amount))
+    client_index = (id * 3) + num_clients
+    counter = 0
+    while counter <= num_operations:
+        if shm[id] == 0:
+            shm[client_index] = random_generator.get_next_number(num_accounts) + 1
+            shm[client_index+1] = random_generator.get_next_number(num_accounts) + 1
+            shm[client_index+2] = random_generator.get_next_number_between(min_transfer_amount, max_transfer_amount)
+            shm[id] = 1
+            counter += 1
+
+    shm[id] = -1
 
 
 if __name__ == "__main__":
@@ -72,19 +87,25 @@ if __name__ == "__main__":
     max_transfer_amount = 300
 
     random_generator = LCG(seed)
-    server_queue = multiprocessing.Queue()
+    shm = multiprocessing.Array('i', [0] * ((num_clients * 3) + num_clients))
 
-    server_process = multiprocessing.Process(target=run_server, args=(server_queue, num_accounts, min_starting_balance, max_starting_balance, random_generator.get_next_number(1000)))
+
+    server_process = multiprocessing.Process(target=run_server, args=(shm, num_accounts, min_starting_balance, max_starting_balance, random_generator.get_next_number(1000), num_clients))
     server_process.start()
 
-    client_processes = [multiprocessing.Process(target=run_client, args=(server_queue, num_operations, num_accounts, min_transfer_amount, max_transfer_amount, random_generator.get_next_number(1000))) for _ in range(num_clients)]
+    client_processes = [
+        multiprocessing.Process(
+            target=run_client,
+            args=(shm, num_operations, num_accounts, min_transfer_amount, max_transfer_amount,
+                  random_generator.get_next_number(1000), i, num_clients)
+        )
+        for i in range(num_clients)
+    ]
 
     for process in client_processes:
         process.start()
 
     for process in client_processes:
         process.join()
-
-    server_queue.put("STOP")
 
     server_process.join()
